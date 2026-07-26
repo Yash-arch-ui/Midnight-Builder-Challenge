8929d3194c1e43d68afd3c575d01fdd8df6f7461
Yash_10 <yashyt01@gmail.com>
2026-07-26 17:55:53 +0000

    Structure as well as path corrected along with packages

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import '@midnight-ntwrk/dapp-connector-api';
+
+function App() {
+  return (
+    <main>
+      <h1>mn-demo</h1>
+      <p>Midnight React frontend scaffold.</p>
+    </main>
+  );
+}
+
+export default App;
